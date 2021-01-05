c9bdabc4604a5a489f09fc2814308413ee1a9357
mahonghui <amisher@163.com>
2021-01-05 21:15:37 +0800

    add requirements

---
+++ app/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# from app.models import Members
+# from app.models import Types
+
+
+admin.site.site_header = '日志分析系统后台管理'
+admin.site.site_title = 'Huawei'
+# admin.site.register(Members)
+# admin.site.register(Types)
+# Register your models here.
